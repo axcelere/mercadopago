461132bd2242669cf28526797713a2f764f73bfd
Rafael Aguero Baquero <raguero@quanam.com>
2024-12-21 13:42:07 -0300

    [UPD] uninstall hook

--- payment_mercadopago/__init__.py
+++ payment_mercadopago/__init__.py
@@ -9,5 +9,5 @@ from . import wizards
 from odoo.addons.payment import reset_payment_provider
 
 
-def uninstall_hook(cr, registry):
-    reset_payment_provider(cr, registry, 'mercadopago')
+def uninstall_hook(env):
+    reset_payment_provider(env, 'mercadopago')
